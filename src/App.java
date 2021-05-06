2d792a1e54b0546aef9797dbde2096df3fbcfedf
yusufakinalp <yusufakinalp@gmail.com>
2021-05-06 22:24:34 +0300

    find possible words

--- src/App.java
+++ src/App.java
@@ -3,12 +3,26 @@ public class App {
         Trie trie = new Trie();
 
         trie.insert("apple");
+        trie.insert("app");
         trie.insert("application");
         trie.insert("apply");
+        trie.insert("ab");
+        trie.insert("aba");
+        trie.insert("abacı");
+        trie.insert("abacılık");
+        trie.insert("abajur");
+        trie.insert("abaküs");
+        trie.insert("abalı");
+        trie.insert("abana");
 
-        System.out.println(trie.contains("app"));   //true
-        System.out.println(trie.contains("apple")); //true
-        System.out.println(trie.contains("appe"));  //false
-        System.out.println(trie.contains("applic"));//true
+        System.out.println(trie.contains("app")); // true
+        System.out.println(trie.contains("apple")); // true
+        System.out.println(trie.contains("appe")); // false
+        System.out.println(trie.contains("applic"));// true
+
+        System.out.println(trie.find("app").text);
+        System.out.println(trie.findPossibles("app"));
+        System.out.println(trie.findPossibles("ab"));
+        System.out.println(trie.findPossibles("abac"));
     }
 }
--- src/Trie.java
+++ src/Trie.java
@@ -9,6 +9,44 @@ public class Trie {
         nodes = new ArrayList<>();
     }
 
+    public List<String> findPossibles(String text) {
+        List<String> wordList = new ArrayList<>();
+        Node node = find(text);
+
+        if (node != null) {
+            getWords(wordList, node.children);
+        }
+
+        return wordList;
+    }
+
+    public void getWords(List<String> wordList, List<Node> nodeList) {
+        if(nodeList.isEmpty()) {
+            return;
+        }
+
+        nodeList.forEach(node -> {if (node.isWord) wordList.add(node.text);});
+
+        nodeList.forEach(node -> getWords(wordList, node.children));
+    }
+
+    public Node find(String text) {
+        List<Node> compareNodes = nodes;
+        for (int i = 0; i < text.length(); i++) {
+            String slice = text.substring(0, i+1);
+            
+            int index = compareNodes.indexOf(new Node(slice));
+            if(index > -1) {
+                if(slice.length() == text.length()) {
+                    return compareNodes.get(index);
+                }
+                compareNodes = compareNodes.get(index).children;
+            }
+        }
+
+        return null;
+    }
+
     public boolean contains(String text) {
         List<Node> compareNodes = nodes;
         for (int i = 0; i < text.length(); i++) {
@@ -29,12 +67,16 @@ public class Trie {
         List<Node> compareNodes = nodes;
         for (int i = 0; i < text.length(); i++) {
             String slice = text.substring(0, i+1);
+            boolean isWord = slice.length() == text.length();
             Node newNode = new Node(slice);
+            newNode.isWord = isWord;
             int index = compareNodes.indexOf(newNode);
             if(index < 0) {
                 compareNodes.add(newNode);
                 compareNodes = newNode.children;
             } else {
+                Node existingNode = compareNodes.get(index);
+                existingNode.isWord = existingNode.isWord || isWord;
                 compareNodes = compareNodes.get(index).children;
             }
         }
